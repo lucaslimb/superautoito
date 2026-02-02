9fbff0068c2720418adea147a61bf73f647ba659
Lucas de Lima <lucasdelimabzr@gmail.com>
2026-02-02 18:37:28 -0300

    first commit

---
+++ app/src/main/java/lucaslimb/com/github/superautoito/MainActivity.kt
@@ -0,0 +1,13 @@
+package lucaslimb.com.github.superautoito
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity {
+
+
+
+}
