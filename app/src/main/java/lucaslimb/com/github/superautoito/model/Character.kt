cef5aa0146d8dfc1943f1d50266e108fd6c9d7c1
Lucas de Lima <lucasdelimabzr@gmail.com>
2026-02-02 22:42:18 -0300

    add: character images

--- app/src/main/java/lucaslimb/com/github/superautoito/model/Character.kt
+++ app/src/main/java/lucaslimb/com/github/superautoito/model/Character.kt
@@ -2,6 +2,7 @@ package lucaslimb.com.github.superautoito.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import lucaslimb.com.github.superautoito.R
 
 enum class CharacterType {
     CHILD,
@@ -21,273 +22,305 @@ enum class TriggerType {
 
 @Parcelize
 data class Character(
-    val id: String,
+    val id: Int,
     val name: String,
     val attack: Int,
     val defense: Int,
     val types: List<CharacterType>,
     val trigger: TriggerType,
-    val imageResId: Int = 0
+    val image: Int = 0
 ) : Parcelable {
 
     companion object {
         fun getDefaultCharacters(): List<Character> {
             return listOf(
                 Character(
-                    id = "char_001",
+                    id = 27,
                     name = "Oshikiri",
                     attack = 2,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.oshikiri
                 ),
                 Character(
-                    id = "char_002",
+                    id = 16,
                     name = "Professor Okabe",
                     attack = 4,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.professor
                 ),
                 Character(
-                    id = "char_003",
+                    id = 12,
                     name = "Moço do Sorvete",
                     attack = 2,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.moco
                 ),
                 Character(
-                    id = "char_004",
+                    id = 8,
                     name = "Kuriko",
                     attack = 2,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.kuriko
                 ),
                 Character(
-                    id = "char_005",
+                    id = 3,
                     name = "Fuchi",
                     attack = 3,
                     defense = 5,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.fuchi
                 ),
                 Character(
-                    id = "char_006",
+                    id = 4,
                     name = "Garoto da Encruzilhada",
                     attack = 2,
                     defense = 3,
                     types = listOf(CharacterType.SUPERNATURAL, CharacterType.CHILD),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.garoto
                 ),
                 Character(
-                    id = "char_007",
+                    id = 19,
                     name = "Yuki",
                     attack = 1,
                     defense = 2,
                     types = listOf(CharacterType.CORPSE),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.yuki
                 ),
                 Character(
-                    id = "char_008",
+                    id = 15,
                     name = "Tio Kingoro",
                     attack = 2,
                     defense = 1,
                     types = listOf(CharacterType.CORPSE),
-                    trigger = TriggerType.ON_PREPARE
+                    trigger = TriggerType.ON_PREPARE,
+                    image = R.drawable.tio
                 ),
                 Character(
-                    id = "char_009",
+                    id = 11,
                     name = "Misuzu",
                     attack = 1,
                     defense = 2,
                     types = listOf(CharacterType.CORPSE),
-                    trigger = TriggerType.BEFORE_BATTLE
+                    trigger = TriggerType.BEFORE_BATTLE,
+                    image = R.drawable.misuzu
                 ),
                 Character(
-                    id = "char_010",
+                    id = 26,
                     name = "Yuko",
                     attack = 2,
                     defense = 1,
                     types = listOf(CharacterType.CHILD),
-                    trigger = TriggerType.BEFORE_BATTLE
+                    trigger = TriggerType.BEFORE_BATTLE,
+                    image = R.drawable.yuko
                 ),
                 Character(
-                    id = "char_011",
+                    id = 5,
                     name = "Mulher das Costelas",
                     attack = 3,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.BEFORE_BATTLE
+                    trigger = TriggerType.BEFORE_BATTLE,
+                    image = R.drawable.mulher
                 ),
                 Character(
-                    id = "char_012",
+                    id = 30,
                     name = "Goro",
                     attack = 1,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.BEFORE_BATTLE
+                    trigger = TriggerType.BEFORE_BATTLE,
+                    image = R.drawable.goro
                 ),
                 Character(
-                    id = "char_013",
+                    id = 31,
                     name = "Binzo",
                     attack = 5,
                     defense = 3,
                     types = listOf(CharacterType.CHILD),
-                    trigger = TriggerType.BEFORE_BATTLE
+                    trigger = TriggerType.BEFORE_BATTLE,
+                    image = R.drawable.binzo
                 ),
                 Character(
-                    id = "char_014",
+                    id = 1,
                     name = "Tomie",
                     attack = 2,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.tomie
                 ),
                 Character(
-                    id = "char_015",
+                    id = 2,
                     name = "Souichi",
                     attack = 3,
                     defense = 2,
                     types = listOf(CharacterType.CHILD),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.souichi
                 ),
                 Character(
-                    id = "char_016",
+                    id = 29,
                     name = "Kazuya Tani",
                     attack = 2,
                     defense = 1,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.kazuya
                 ),
                 Character(
-                    id = "char_017",
+                    id = 22,
                     name = "Terumi",
                     attack = 1,
                     defense = 3,
                     types = listOf(CharacterType.SUPERNATURAL, CharacterType.CORPSE),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.terumi
                 ),
                 Character(
-                    id = "char_018",
+                    id = 6,
                     name = "Chiemi",
                     attack = 3,
                     defense = 1,
                     types = listOf(CharacterType.CORPSE, CharacterType.SUPERNATURAL),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.chiemi
                 ),
                 Character(
-                    id = "char_019",
+                    id = 9,
                     name = "Soldado Furukawa",
                     attack = 2,
                     defense = 2,
                     types = listOf(CharacterType.CORPSE),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.soldado
                 ),
                 Character(
-                    id = "char_020",
+                    id = 24,
                     name = "Kumi",
                     attack = 1,
                     defense = 2,
                     types = listOf(CharacterType.CORPSE),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.kumi
                 ),
                 Character(
-                    id = "char_021",
+                    id = 7,
                     name = "Reanimador",
                     attack = 4,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.reanimador
                 ),
                 Character(
-                    id = "char_022",
+                    id = 23,
                     name = "Maya",
                     attack = 2,
                     defense = 1,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.maya
                 ),
                 Character(
-                    id = "char_023",
+                    id = 14,
                     name = "Nakayama",
                     attack = 1,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.nakayama
                 ),
                 Character(
-                    id = "char_024",
+                    id = 10,
                     name = "Manchas do Beco",
                     attack = 3,
                     defense = 3,
                     types = listOf(CharacterType.CORPSE, CharacterType.SUPERNATURAL),
-                    trigger = TriggerType.ON_DEATH
+                    trigger = TriggerType.ON_DEATH,
+                    image = R.drawable.manchas
                 ),
                 Character(
-                    id = "char_025",
+                    id = 32,
                     name = "Misaki",
                     attack = 3,
                     defense = 2,
                     types = listOf(CharacterType.CHILD),
-                    trigger = TriggerType.ON_KILL
+                    trigger = TriggerType.ON_KILL,
+                    image = R.drawable.misaki
                 ),
                 Character(
-                    id = "char_026",
+                    id = 21,
                     name = "Numei",
                     attack = 2,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_KILL
+                    trigger = TriggerType.ON_KILL,
+                    image = R.drawable.numei
                 ),
                 Character(
-                    id = "char_027",
+                    id = 20,
                     name = "Ryo Tsukano",
                     attack = 3,
                     defense = 1,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_KILL
+                    trigger = TriggerType.ON_KILL,
+                    image = R.drawable.ryo
                 ),
                 Character(
-                    id = "char_028",
+                    id = 28,
                     name = "Frankenstein",
                     attack = 3,
                     defense = 3,
                     types = listOf(CharacterType.SUPERNATURAL),
-                    trigger = TriggerType.ON_KILL
+                    trigger = TriggerType.ON_KILL,
+                    image = R.drawable.frank
                 ),
                 Character(
-                    id = "char_029",
+                    id = 25,
                     name = "Ayumi",
                     attack = 3,
                     defense = 3,
                     types = listOf(CharacterType.CORPSE, CharacterType.SUPERNATURAL),
-                    trigger = TriggerType.ON_KILL
+                    trigger = TriggerType.ON_KILL,
+                    image = R.drawable.ayumi
                 ),
                 Character(
-                    id = "char_030",
+                    id = 17,
                     name = "Esculturas Sem Cabeça",
                     attack = 4,
                     defense = 3,
                     types = listOf(CharacterType.SUPERNATURAL),
-                    trigger = TriggerType.ON_KILL
+                    trigger = TriggerType.ON_KILL,
+                    image = R.drawable.esculturas
                 ),
                 Character(
-                    id = "char_031",
+                    id = 18,
                     name = "Hideo",
                     attack = 2,
                     defense = 1,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.ON_KILL
+                    trigger = TriggerType.ON_KILL,
+                    image = R.drawable.hideo
                 ),
                 Character(
-                    id = "char_032",
+                    id = 13,
                     name = "Tomoo",
                     attack = 3,
                     defense = 2,
                     types = listOf(CharacterType.COMMON),
-                    trigger = TriggerType.AFTER_BATTLE
+                    trigger = TriggerType.AFTER_BATTLE,
+                    image = R.drawable.tomoo
                 )
             )
         }
